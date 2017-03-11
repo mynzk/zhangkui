5a22158e2968d2cca6d28dd79973d612a8d87e76
zhangkui <zhangkui@qq.com>
2017-03-11 17:13:21 +0800

    eslint的学习

---
+++ public-eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": "eslint:recommended",
+    "env":{
+         'node':true
+    },
+    "rules":{
+    	'no-console':0,
+    	'indent': [ 'error', 2 ],
+        'quotes': [ 'error', 'single' ],
+    }
+};
